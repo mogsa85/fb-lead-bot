f0ecc299f94cde8d91c64f1c1d108b1bfc112718
mogsa85 <robertmogsa85@gmail.com>
2026-03-21 11:36:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.post("/api/replies", (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
