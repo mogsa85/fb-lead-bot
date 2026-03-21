99b7b8f6c3b5ab76f0ea2937dff673d9da225367
mogsa85 <robertmogsa85@gmail.com>
2026-03-21 12:04:37 +0000

    Create mailer.js

---
+++ mailer.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ionos.co.uk",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "info@jarholidays.co.uk",
+    pass: "Safety99?!?"
+  }
+});
+
+async function sendEmail(subject, message) {
+  try {
+    await transporter.sendMail({
+      from: '"Lead Bot" <info@jarholidays.co.uk>',
+      to: "info@jarholidays.co.uk",
+      subject: subject,
+      text: message
+    });
+
+    console.log("📧 Email sent");
+  } catch (err) {
+    console.error("Email error:", err);
+  }
+}
+
+module.exports = sendEmail;
