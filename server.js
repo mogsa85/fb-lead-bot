1ea62ccfaf2eb57731ad6acd544fda982e4dadb9
mogsa85 <robertmogsa85@gmail.com>
2026-03-21 11:34:00 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+
+const DATA_FILE = "./data.json";
+
+function loadData() {
+  return JSON.parse(fs.readFileSync(DATA_FILE));
+}
+
+function saveData(data) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+}
+
+app.get("/api/data", (req, res) => {
+  res.json(loadData());
+});
+
+app.post("/api/groups", (req, res) => {
+  const data = loadData();
+  data.groups.push(req.body.group);
+  saveData(data);
+  res.json({ success: true });
+});
+
+app.post("/api/keywords", (req, res) => {
+  const data = loadData();
+  data.keywords.push(req.body.keyword);
+  saveData(data);
+  res.json({ success: true });
+});
+
+app.post("/api/replies", (req, res) => {
+  const data = loadData();
+  data.replies.push(req.body.reply);
+  saveData(data);
+  res.json({ success: true });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
