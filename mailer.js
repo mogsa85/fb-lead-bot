7f9bf3322ffb7cb614956eb06c4f0d56a8549149
mogsa85 <robertmogsa85@gmail.com>
2026-03-21 16:48:42 +0000

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -2,26 +2,31 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.ionos.co.uk",
-  port: 465,
+  port: 587,
   secure: false,
   auth: {
     user: "info@jarholidays.co.uk",
     pass: "Safety99?!?"
-  }
+  },
+  connectionTimeout: 5000, // 5 seconds
+  greetingTimeout: 5000,
+  socketTimeout: 5000
 });
 
 async function sendEmail(subject, message) {
   try {
-    await transporter.sendMail({
-      from: '"Lead Bot" <info@jarholidays.co.uk>',
+    console.log("📧 Trying to send email...");
+
+    const info = await transporter.sendMail({
+      from: '"Jar Holidays Leads" <info@jarholidays.co.uk>',
       to: "info@jarholidays.co.uk",
       subject: subject,
       text: message
     });
 
-    console.log("📧 Email sent");
+    console.log("✅ Email sent:", info.response);
   } catch (err) {
-    console.error("Email error:", err);
+    console.error("❌ Email failed:", err.message);
   }
 }
 
